bf0f85be15bd4548a3c6a34aff3148156c8e860c
slavik <slavikrovny@gmail.com>
2018-08-24 16:02:45 +0300

    Must do chatting Activity

---
+++ app/src/main/java/com/example/slavick/firebasechat/LoginActivity.java
@@ -0,0 +1,51 @@
+package com.example.slavick.firebasechat;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class LoginActivity extends AppCompatActivity {
+    @BindView(R.id.passwordLogin)
+    EditText passwordLogin;
+    @BindView(R.id.e_mailAddress)
+    EditText emailAddress;
+    @BindView(R.id.sthWrong)
+    TextView sthWrong;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.login)
+    public void onClick() {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            sthWrong.setText("This account already in use at the moment!");
+        } else {
+            auth.signInWithEmailAndPassword(emailAddress.getText().toString(), passwordLogin.getText().toString())
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                            } else {
+                                sthWrong.setText("Check out your email and password and try again");
+                            }
+                        }
+                    });
+        }
+    }
+}
---
+++ app/src/main/java/com/example/slavick/firebasechat/RegistrationActivity.java
@@ -0,0 +1,63 @@
+package com.example.slavick.firebasechat;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class RegistrationActivity extends AppCompatActivity {
+    @BindView(R.id.e_mail)
+    EditText email;
+    @BindView(R.id.password)
+    EditText password;
+    @BindView(R.id.passwordRepeat)
+    EditText passwordRepeat;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.createAcc)
+    public void onClick(){
+        if (isPasswordValid(password.getText().toString())) {
+            FirebaseAuth.getInstance().
+                    createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        }
+                    });
+        }
+
+    }
+    @OnClick(R.id.haveAcc)
+    public void onSecondClick(){
+        Intent intent =  new Intent();
+
+    }
+    public boolean isPasswordValid(String password){
+        boolean pass = false;
+        if (password.length() > 6 & password.equals(passwordRepeat.getText().toString())){
+            pass = true;
+        }
+        return pass;
+    }
+}
